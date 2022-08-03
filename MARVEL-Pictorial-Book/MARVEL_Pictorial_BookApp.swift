274dec1b9a67ec0e90d68d0d59a8dec1dc7495e4
RyuseiHanado <riyusei_hanado@mail.toyota.co.jp>
2022-08-04 07:37:37 +0900

    Initial Commit

---
+++ MARVEL-Pictorial-Book/MARVEL_Pictorial_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MARVEL_Pictorial_BookApp.swift
+//  MARVEL-Pictorial-Book
+//
+//  Created by 花堂　瑠聖 on 2022/08/04.
+//
+
+import SwiftUI
+
+@main
+struct MARVEL_Pictorial_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
