18f7d127975e3e44c75d73fc4c06af5d0ee98321
RyuseiHanado <riyusei_hanado@mail.toyota.co.jp>
2022-08-04 20:45:56 +0900

    Add .gitignore File

---
+++ MARVEL-Pictorial-Book/Models/ImageDownloader.swift
@@ -0,0 +1,24 @@
+//
+//  ImageDownloader.swift
+//  MARVEL-Pictorial-Book
+//
+//  Created by 花堂　瑠聖 on 2022/08/04.
+//
+
+import Foundation
+
+class ImageDownloader : ObservableObject {
+    @Published var downloadData: Data? = nil
+
+    func downloadImage(url: String) {
+
+        guard let imageURL = URL(string: url) else { return }
+
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: imageURL)
+            DispatchQueue.main.async {
+                self.downloadData = data
+            }
+        }
+    }
+}
